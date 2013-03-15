eafb3b5f43baac06511914ca77f243bc111e3309
mntjcmrc <mntjcmrc@gmail.com>
2013-03-15 19:12:16 +0100

    datos() modificado para que el modelo se añada también en el concesionario

--- examen2/src/examen2/Modelo.java
+++ examen2/src/examen2/Modelo.java
@@ -3,19 +3,19 @@ package examen2;
 import rutinas.Rutinas;
 
 public class Modelo {
-	
+
 	// Atributos
 	private int _codigoModelo;
 	private String _descripcion;
 	private float _pvp;
 
 	// Constructor vacío
-	public Modelo(){
-		
+	public Modelo() {
+
 	}
 
 	// Getters y setters
-	
+
 	public int get_codigoModelo() {
 		return _codigoModelo;
 	}
@@ -39,9 +39,7 @@ public class Modelo {
 	public void set_pvp(float _pvp) {
 		this._pvp = _pvp;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Codigo modelo: " + get_codigoModelo() + "\nDescripcion: "
@@ -49,30 +47,48 @@ public class Modelo {
 	}
 
 	// Pide los datos al usuario
-	public void datos(Modelos modelos){
+	public void datos(Modelos modelos, Concesionarios concesionarios) {
 		int codigoModelo = -1;
 		String descripcion = "";
 		float pvp = 0;
-		
+
 		boolean error = true;
-		
-		while(error){
+
+		while (error) {
 			error = false;
-			codigoModelo = Rutinas.leeEntero("Introduce el codigo del modelo: ");
-			if(modelos.existeModelo(codigoModelo)){
+			codigoModelo = Rutinas
+					.leeEntero("Introduce el codigo del modelo: ");
+			if (modelos.existeModelo(codigoModelo)) {
 				error = true;
 				System.out.println("El codigo introducido ya existe");
 			}
 		}
-		descripcion = Rutinas.leeString("Introduce la descripción del modelo: ");
+		descripcion = Rutinas
+				.leeString("Introduce la descripción del modelo: ");
 		pvp = Rutinas.leeFloat("Introduce el precio del modelo: ");
-		
+
 		this.set_codigoModelo(codigoModelo);
 		this.set_descripcion(descripcion);
 		this.set_pvp(pvp);
-		
+
 		modelos.add(this);
+		int codConc = -1;
+		Concesionario concesionario = new Concesionario();
+		codConc = Rutinas
+				.leeEntero("Código del concesionario en el que se añadirá el coche: ");
+		error = true;
+
+		while (error) {
+			error = false;
+			if (concesionarios.existeConcesionario(codConc)) {
+				concesionario = concesionarios.buscar(codConc);
+			} else {
+				error = true;
+				System.out
+						.println("Ese código no corresponde a ningún concesionario");
+			}
+		}
+		concesionario.addModelo(this);
 	}
 
-	
 }
