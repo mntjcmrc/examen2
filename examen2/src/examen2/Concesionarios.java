5554890eab7a2baad569f0b09c5083f7321a3107
mntjcmrc <mntjcmrc@gmail.com>
2013-03-15 16:58:23 +0100

    guardar() y cargar()

--- examen2/src/examen2/Concesionarios.java
+++ examen2/src/examen2/Concesionarios.java
@@ -1,5 +1,11 @@
 package examen2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Concesionarios {
@@ -20,6 +26,42 @@ public class Concesionarios {
 
 	public void set_concesionarios(ArrayList<Concesionario> _concesionarios) {
 		this._concesionarios = _concesionarios;
+	}
+	
+	// Guarda el ArrayList de modelos en un archivo llamado "concesionarios.obj"
+	public void guardar(){
+		ArrayList<Concesionario> modelos = this.get_concesionarios();
+		if(modelos.size() == 0){
+			System.out.println("No hay datos de concesionarios para guardar");
+		} else {
+			try {
+				ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream("concesionarios.obj"));
+				salida.writeObject(modelos);
+				salida.close();
+				System.out.println("-- Concesionarios guardados con éxito --");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// Carga el archivo "concesionarios.obj"
+	@SuppressWarnings("unchecked")
+	public void cargar() {
+		File f = new File("concesionarios.obj");
+		if (f.exists()) {
+			try {
+				ObjectInputStream entrada = new ObjectInputStream(
+						new FileInputStream("concesionarios.obj"));
+				this._concesionarios = (ArrayList<Concesionario>) entrada.readObject();
+				entrada.close();
+				System.out.println("-- Concesionarios cargados con éxito --");
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("El fichero no existe");
+		}
 	}
 	
 }
--- examen2/src/examen2/Modelos.java
+++ examen2/src/examen2/Modelos.java
@@ -1,5 +1,11 @@
 package examen2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Modelos {
@@ -21,4 +27,40 @@ public class Modelos {
 //	private void set_modelos(ArrayList<Modelo> _modelos) {
 //		this._modelos = _modelos;
 //	}
+	
+	// Guarda el ArrayList de modelos en un archivo llamado "modelos.obj"
+	public void guardar(){
+		ArrayList<Modelo> modelos = this.get_modelos();
+		if(modelos.size() == 0){
+			System.out.println("No hay datos de concesionarios para guardar");
+		} else {
+			try {
+				ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream("modelos.obj"));
+				salida.writeObject(modelos);
+				salida.close();
+				System.out.println("-- Modelos guardados con éxito --");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// Carga el archivo "modelos.obj"
+	@SuppressWarnings("unchecked")
+	public void cargar() {
+		File f = new File("modelos.obj");
+		if (f.exists()) {
+			try {
+				ObjectInputStream entrada = new ObjectInputStream(
+						new FileInputStream("modelos.obj"));
+				this._modelos = (ArrayList<Modelo>) entrada.readObject();
+				entrada.close();
+				System.out.println("-- Modelos cargados con éxito --");
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("El fichero no existe");
+		}
+	}
 }
